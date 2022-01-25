cc0e32b21a4d2d7d4329aea8ca96ec33ee879570
Léon Tran-Van <ephalys@live.fr>
2022-01-25 02:24:28 +0100

    update home text

--- src/components/HomeText.vue
+++ src/components/HomeText.vue
@@ -4,7 +4,9 @@ const title =
 
 const paragraphs = [
   "I began my career with an internship as fullstack developer at Classicat's. My objectives was to create a new site from scratch with an administration interface and increase traffic using good SEO practices.",
-  "Then, I was full-stack developer at Subskill and for them I have worked on projects involving several fields, from luxury fashion brands to banks, from public services to health insurance, to major retail distributors and ecommerce."
+  "Then, I was full-stack developer at Subskill and for them I have worked on projects involving several fields, from luxury fashion brands to banks, from public services to health insurance, to major retail distributors and ecommerce.",
+  "I'm always taking advantage of new technologies and current design trends in order to deliver the perfect product to my clients. That's why JAMstack and Figma take an important part in my workflow.",
+  "In my spare time, I enjoy playing some video games, go play billiards, going to the gym and do motorcycle rides with friends."
 ]
 </script>
 
