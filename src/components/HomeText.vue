ac548b9460e0161be84a13983c0078d446fbdcbe
Léon Tran-Van <ephalys@live.fr>
2022-01-25 02:07:18 +0100

    import signature & setup home text

---
+++ src/components/HomeText.vue
@@ -0,0 +1,18 @@
+<script setup>
+const title =
+  "Hi, I’m Léon Tran-Van. I’m currently working as a front-end developer at Hager, a leading supplier of solutions and services for electrical installations.";
+
+const paragraphs = [
+  "I began my career with an internship as fullstack developer at Classicat's. My objectives was to create a new site from scratch with an administration interface and increase traffic using good SEO practices.",
+  "Then, I was full-stack developer at Subskill and for them I have worked on projects involving several fields, from luxury fashion brands to banks, from public services to health insurance, to major retail distributors and ecommerce."
+]
+</script>
+
+<template>
+    <h1 class="text-2xl font-bold">
+      {{title}}
+    </h1>
+    <p v-for="(paragraph, index) in paragraphs" :key="index" class="text-base">
+      {{paragraph}}
+    </p>
+</template>
