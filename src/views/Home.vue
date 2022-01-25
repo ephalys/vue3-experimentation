b8d4a46f38b966bb7a26bf398a12bc892475b649
Léon Tran-Van <ephalys@live.fr>
2022-01-25 04:51:49 +0100

    improve images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <script setup>
 import HomeText from "@/components/HomeText.vue";
 import Socials from "@/components/Socials.vue";
-import imageOfMe from "@/images/leon-tran-van.png";
+import imageOfMe from "@/images/leon-tran-van.webp";
 import signature from "@/images/signature.svg";
 </script>
 
 <template>
-  <img :src="signature" alt="signature" class="h-12 mb-6 md:mb-12" />
+  <img :src="signature" alt="signature" class="mb-6 md:mb-12" height="12" />
   <HomeText class="order-1 md:w-1/2" />
   <div
     class="
